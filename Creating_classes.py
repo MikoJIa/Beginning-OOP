af229230fb9249dc555b99fb526bb4ddd7599566
MikoJIa <Kolya_popov_86@tut.by>
2023-04-19 21:58:01 +0300

    create class Car add commit class Purse

--- Creating_classes.py
+++ Creating_classes.py
@@ -60,39 +60,55 @@
    атрибуты - все имена в классе: переменных и методов
 '''
 
-class Purse:
+# class Purse:
+#
+#     def __init__(self, valuta, name='unknown'):
+#         if valuta not in ('USD', 'EUR'):
+#             raise ValueError
+#         self.__money = 0.00  # к этому атрибуту мы теперь сможем получить доступ только из
+#                              # класса
+#         self.valuta = valuta
+#         self.name = name
+#
+#     def top_up_balance(self, howmany):
+#         self.__money = self.__money + howmany
+#         return howmany
+#
+#     def top_down_balance(self, howmany):
+#         if self.__money - howmany < 0:
+#             print('Не достаточно средств!!!')
+#             raise ValueError('Не достаточно средств!!!')
+#         self.__money = self.__money - howmany
+#         return howmany
+#
+#     def info(self):
+#         return self.__money
+#
+#     def __del__(self):  # Деструктор
+#         print('Кошелёк удалён')
+#
+#
+# object1 = Purse('USD')  # Кошелёк 1
+# object2 = Purse('USD', 'Bill')  # Кошелёк 2
+# object2.top_up_balance(100)
+# object1.top_up_balance(object2.top_down_balance(35))
+# print(object1.info())
+# print(object2.info())
+# print(object1.__dict__)
 
-    def __init__(self, valuta, name='unknown'):
-        if valuta not in ('USD', 'EUR'):
-            raise ValueError
-        self.__money = 0.00  # к этому атрибуту мы теперь сможем получить доступ только из
-                             # класса
-        self.valuta = valuta
-        self.name = name
 
-    def top_up_balance(self, howmany):
-        self.__money = self.__money + howmany
-        return howmany
+class Car:
+    model = 'BMW'
+    engine = 1.6
 
-    def top_down_balance(self, howmany):
-        if self.__money - howmany < 0:
-            print('Не достаточно средств!!!')
-            raise ValueError('Не достаточно средств!!!')
-        self.__money = self.__money - howmany
-        return howmany
-
-    def info(self):
-        return self.__money
-
-    def __del__(self):  # Деструктор
-        print('Кошелёк удалён')
+    def drive(self):
+        print('Lets go')
 
 
-object1 = Purse('USD')  # Кошелёк 1
-object2 = Purse('USD', 'Bill')  # Кошелёк 2
-object2.top_up_balance(100)
-object1.top_up_balance(object2.top_down_balance(35))
-print(object1.info())
-print(object2.info())
+print(Car.__dict__)
+
+car1 = Car()
+getattr(car1, 'drive')()
+getattr(Car, 'drive')
 
 
