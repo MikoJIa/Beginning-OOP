538a21a3f96e4cfb5be5ad2f991caaee2ba995f1
MikoJIa <Kolya_popov_86@tut.by>
2023-05-16 19:49:40 +0300

    create new directory decorator property and add task1, task2

---
+++ Task_OOP_Property_decorator/Task1.py
@@ -0,0 +1,26 @@
+# конструктор __init__, принимающий список записей, в нем могут находиться любые значения.
+# Необходимо сохранить его в защищенном атрибуте ._notes
+# свойство notes_list, которое распечатает содержимое атрибута
+# ._notes в виде упорядоченного списка (см. пример ниже)
+# note = Notebook(['Buy Potato', 'Buy Carrot', 'Wash car'])
+# note.notes_list
+# После этого на экране вы должны увидеть:
+#
+# 1.Buy Potato
+# 2.Buy Carrot
+# 3.Wash car
+
+class Notebook:
+    def __init__(self, notes):
+        self._notes = notes
+
+    @property
+    def notes_list(self, count=0):
+        for i in self._notes:
+            if i:
+                count += 1
+            print(f"{count}.{i}")
+
+
+note = Notebook(["Buy Potato", "Buy Carrot", "Wash car"])
+note.notes_list
