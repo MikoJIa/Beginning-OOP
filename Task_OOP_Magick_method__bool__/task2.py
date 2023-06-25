e7ea13baca16a5279d2f0419ef0cb98f07d9a698
MikoJIa <Kolya_popov_86@tut.by>
2023-06-25 18:32:07 +0300

    create directory Task_OOP_Magick_method__bool__ and task1, task2, task3

---
+++ Task_OOP_Magick_method__bool__/task2.py
@@ -0,0 +1,45 @@
+# конструктор __init__, принимающий единственный аргумент - название города.
+# Вам необходимо сохранить его в качестве атрибута экземпляра name, причем вам нужно
+# преобразовать переданное имя города таким образом, чтобы первая буква каждого слова была
+# заглавной, а остальные оказались строчными (пример "new york" - > "New York")
+# магический метод __str__ таким образом, чтобы он возвращал имя города
+# магический метод __bool__ так, чтобы он возвращал False ,если название города
+# заканчивается на любую гласную букву латинского алфавита (a, e, i, o, u), в противном
+# случае True
+# Sample Input:
+# Sample Output:
+# New York
+# San Francisco
+# Nizhny Novgorod
+
+
+class City:
+
+    def __init__(self, name):
+        self.name = name.title()
+
+    def __str__(self):
+        return self.name
+
+    def __bool__(self):
+        return False if self.name[-1] in ['a', 'e', 'i', 'o', 'u'] else True
+
+
+p1 = City('new york')
+assert p1.name == "New York"
+assert p1.__str__() == "New York"
+assert isinstance(p1, City)
+print(p1)
+assert bool(p1)
+
+p2 = City('SaN frANCISco')
+assert isinstance(p2, City)
+assert p2.name == "San Francisco"
+print(p2)
+assert not bool(p2)
+
+p3 = City('NIZHNY NoVGORod')
+assert p3.name == "Nizhny Novgorod"
+print(p3)
+assert bool(p3)
+assert isinstance(p3, City)
